4892f437c3a1ef720926ac14d6762f5c81b3a109
yoonho <yoonho0922@naver.com>
2020-08-18 18:11:07 +0900

    face detection

--- app/src/main/java/com/example/blinkmeasure/MainActivity.java
+++ app/src/main/java/com/example/blinkmeasure/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.blinkmeasure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.blinkmeasure.facedetector.FaceDetectorProcessor;
+import com.example.blinkmeasure.preference.PreferenceUtils;
+import com.google.mlkit.common.model.LocalModel;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.label.custom.CustomImageLabelerOptions;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions;
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FACE_DETECTION = "Face Detection";
+    private static final String TAG = "Main";
+
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        preview = findViewById(R.id.preview);
+        graphicOverlay = findViewById(R.id.graphic_overlay);
+
+        createCameraSource(FACE_DETECTION);
+    }
+
+    private void createCameraSource(String model) {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, graphicOverlay);
+        }
+
+        try {
+            Log.i(TAG, "Using Face Detector Processor");
+            FaceDetectorOptions faceDetectorOptions =
+                    PreferenceUtils.getFaceDetectorOptionsForLivePreview(this);
+            cameraSource.setMachineLearningFrameProcessor(
+                    new FaceDetectorProcessor(this, faceDetectorOptions));
+
+        } catch (Exception e) {
+            Log.e(TAG, "Can not create image processor: " + model, e);
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Can not create image processor: " + e.getMessage(),
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
