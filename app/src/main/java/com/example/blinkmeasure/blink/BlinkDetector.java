51c091c0c39bff5dc179a6bf9fde63398e67b809
yoonho <yoonho0922@naver.com>
2020-08-25 14:26:34 +0900

    로그 수정

--- app/src/main/java/com/example/blinkmeasure/blink/BlinkDetector.java
+++ app/src/main/java/com/example/blinkmeasure/blink/BlinkDetector.java
@@ -2,29 +2,38 @@ package com.example.blinkmeasure.blink;
 
 import android.util.Log;
 
-public class BlinkDetection {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    private static int WINDOW_SIZE = 30;
-    private static double EAR_THRESH = 0.2;
-    private static int EAR_CONSEC_FRAMES = 3;
+public class BlinkDetector {
+
+    private static double EAR_THRESH = 0.18;
+    private static int EAR_CONSEC_FRAMES = 1;
 
     private int counter;
     private int blinkNumber;
 
-    public BlinkDetection(){
+    public BlinkDetector(){
         counter = 0;
         blinkNumber = 0;
     }
 
     public int detect(double EAR) {
+        Log.i("get_EAR", "" + EAR);
+
+        long now = System.currentTimeMillis();
+        Date mDate = new Date(now);
+        SimpleDateFormat simpleDate = new SimpleDateFormat("HH-mm-ss.SSS");
+        String getTime = simpleDate.format(mDate);
+        Log.i("detect_time", getTime);
 
         if(EAR < EAR_THRESH) {
             counter++;
-            Log.i("BlinkDetection", "eye close - " + EAR + ", " + counter);
+            Log.i("eye_close", counter + ", " + EAR);
         } else {
             if(counter > EAR_CONSEC_FRAMES){
                 blinkNumber++;
-                Log.i("BlinkDetection", "Blink - " + blinkNumber);
+                Log.i("blink_number", "" + blinkNumber);
             }
 
             counter = 0;
